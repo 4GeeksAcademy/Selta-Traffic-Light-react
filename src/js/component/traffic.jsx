057ebebb9510d4decd6ba2fa4e7b7c824fd06318
Selta7 <132694122+Selta7@users.noreply.github.com>
2023-11-11 14:23:19 +0000

    was able to cycle through colors

--- src/js/component/traffic.jsx
+++ src/js/component/traffic.jsx
@@ -1,24 +1,36 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 const Traffic = () => {
+  const [colors, setColor] = useState(["red", "yellow", "green"]);
+  const [currentColor, setCurrentColor] = useState(0);
 
-    const [ colors , setColor] = useState (["red","yellow","green"])
-    const [currentColor, setCurrentColor] = useState("red");
-    
+  const changeLight = () => {
+    // Increment the index, and wrap around if it goes beyond the array length
+    setCurrentColor((prevIndex) => (prevIndex + 1) % colors.length);
+  };
 
-
-    return (
-        <div clasName="container" >
-        <div className="trafficLight">
-        { colors.map (( color ) => (<span 
-        key= {color}
-        className={` ${color} ${currentColor === color ? 'glow' : ''}`}
-        onClick={() => setCurrentColor(color)}> </span>  ))}
-        </div>
-        </div>
-    );
+  return (
+    <div className="container">
+      <div className="trafficLight">
+        {colors.map((color, index) => (
+          <span
+            key={color}
+            className={` ${color} ${
+                currentColor === index ? "glow" : ""
+            }`}
+            onClick={() => setCurrentColor(index)}
+          ></span>
+        ))}
+      </div>
+      <button
+        type="button"
+        className="btn btn-success"
+        onClick={changeLight}
+      >
+        Change Light
+      </button>
+    </div>
+  );
 };
-
 
 export default Traffic;
